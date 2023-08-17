de55cf694af37d60d6cbe96c987bd2f6c8021036
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-17 02:57:55 +0000

    Test Folder

--- Arrays_and_hashmaps/contains_duplicate_217.py
+++ Arrays_and_hashmaps/contains_duplicate_217.py
@@ -20,22 +20,3 @@ class Solution:
 
             hashset.add(n)
         return False
-
-class Testing:
-    def test_example_1(self):
-        solution = Solution()
-        nums = [1, 2, 3, 1]
-        assert solution.containsDuplicate(nums) == True
-    
-    def test_example_2(self):
-        solution = Solution()
-        nums = [1,2,3,4]
-        assert solution.containsDuplicate(nums) == False
-    
-    def test_example_3(self):
-        solution = Solution()
-        nums = [1,1,1,3,3,4,3,2,4,2]
-        assert solution.containsDuplicate(nums) == True
-
-if __name__ == "__main__":
-    pytest.main()
---
+++ Arrays_and_hashmaps/tests/test_contains_duplicate_217.py
@@ -0,0 +1,25 @@
+import pytest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from contains_duplicate_217 import Solution
+
+class Testing:
+    def test_example_1(self):
+        solution = Solution()
+        nums = [1, 2, 3, 1]
+        assert solution.containsDuplicate(nums) == True
+    
+    def test_example_2(self):
+        solution = Solution()
+        nums = [1,2,3,4]
+        assert solution.containsDuplicate(nums) == False
+    
+    def test_example_3(self):
+        solution = Solution()
+        nums = [1,1,1,3,3,4,3,2,4,2]
+        assert solution.containsDuplicate(nums) == True
+
+if __name__ == "__main__":
+    pytest.main()
