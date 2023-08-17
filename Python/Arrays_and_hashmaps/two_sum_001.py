fb474f36a359adcbff6902efdb2a96383b51d72d
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-17 14:03:13 +0530

    two sum

---
+++ Python/Arrays_and_hashmaps/two_sum_001.py
@@ -0,0 +1,21 @@
+"""
+Two Sum
+Time Complexity O(n)
+Space Complexity O(n)
+"""
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in hashmap:
+                return [hashmap[diff], i]
+
+            hashmap[n] = i
+        
+        return []
