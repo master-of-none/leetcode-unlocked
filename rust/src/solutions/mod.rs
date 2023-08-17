9cd1858daddb50aa296587e2383afe14f6a52655
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-17 14:21:48 +0530

    Problem 217

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -1,4 +1,4 @@
 pub mod p001_two_sum;
-
+pub mod p217_contains_duplicates;
 // Re-export the modules for external use
 //pub use p001_two_sum::*;
---
+++ rust/src/solutions/p217_contains_duplicates.rs
@@ -0,0 +1,40 @@
+// Contains Duplicates
+use std::collections::HashSet;
+
+pub fn contains_duplicates(nums: Vec<i32>) -> bool {
+    let mut hashset: HashSet<i32> = HashSet::new();
+
+    for n in nums {
+        if hashset.contains(&n) {
+            return true;
+        }
+        hashset.insert(n);
+    }
+    false
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let nums = vec![1, 2, 3, 1];
+        let result = contains_duplicates(nums);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_2() {
+        let nums = vec![1, 2, 3, 4];
+        let result = contains_duplicates(nums);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_3() {
+        let nums = vec![1, 2, 3, 1, 2, 3, 1, 5, 6, 2, 3];
+        let result = contains_duplicates(nums);
+        assert_eq!(result, true);
+    }
+}
