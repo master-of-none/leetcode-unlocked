983530f123bca87150a41cddb4532be37412e274
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-17 22:18:51 +0530

    Problem 36

---
+++ rust/src/solutions/p036_valid_sudoku.rs
@@ -0,0 +1,70 @@
+// Valid Sudoku
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub fn valid_sudoku(board: Vec<Vec<char>>) -> bool {
+    let mut rows: HashMap<usize, HashSet<char>> = HashMap::new();
+    let mut cols: HashMap<usize, HashSet<char>> = HashMap::new();
+    let mut squares: HashMap<(usize, usize), HashSet<char>> = HashMap::new();
+
+    for r in 0..9 {
+        for c in 0..9{
+            let cell = board[r][c];
+
+            if cell == '.' {
+                continue;
+            }
+
+            if rows.entry(r).or_insert(HashSet::new()).contains(&cell)
+                || cols.entry(c).or_insert(HashSet::new()).contains(&cell)
+                || squares.entry((r / 3, c / 3)).or_insert(HashSet::new()).contains(&cell) {
+                    return false;
+                }
+            
+            rows.entry(r).or_insert(HashSet::new()).insert(cell);
+            cols.entry(c).or_insert(HashSet::new()).insert(cell);
+            squares.entry((r / 3, c / 3)).or_insert(HashSet::new()).insert(cell);
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let board: Vec<Vec<char>> = vec![
+            vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+        ];
+
+        assert_eq!(valid_sudoku(board), true);
+    }
+
+    #[test]
+    fn test_2() {
+        let board: Vec<Vec<char>> = vec![
+            vec!['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+        ];
+
+        assert_eq!(valid_sudoku(board), false);
+    }
+}
