d2d55b4a2af530ccc39231d4df8379e48e35c873
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-17 19:28:06 +0530

    Problem 49

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -1,6 +1,7 @@
 pub mod p001_two_sum;
 pub mod p217_contains_duplicates;
 pub mod p242_valid_anagram;
+pub mod p049_group_anagrams;
 
 // Re-export the modules for external use
 //pub use p001_two_sum::*;
