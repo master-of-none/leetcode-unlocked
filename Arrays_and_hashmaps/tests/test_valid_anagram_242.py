a83c1a37f09953dae910b594a9080b5b3dd3a4b3
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-17 03:35:33 +0000

    Valid Anagram

---
+++ Arrays_and_hashmaps/tests/test_valid_anagram_242.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pytest
+
+from valid_anagram_242 import Solution
+
+class Testing:
+    def test_example_1(self):
+        solution = Solution()
+        s = "anagram"
+        t = "nagaram"
+        assert solution.isAnagram(s,t) == True
+    
+    def test_example_2(self):
+        solution = Solution()
+        s = "rat"
+        t = "car"
+        assert solution.isAnagram(s,t) == False
+
+if __name__ == "__main__":
+    pytest.main()
+
