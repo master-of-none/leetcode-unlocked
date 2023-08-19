2ba05724faacc9af9d70df420b2b881baafd7ab7
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-19 12:11:07 +0530

    Problem 11

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -8,6 +8,7 @@ pub mod p128_longest_consecutive_sequence;
 pub mod p125_valid_palindrome;
 pub mod p167_two_sum_ii;
 pub mod p015_3sum;
+pub mod p011_container_water;
 
 // Re-export the modules for external use
 //pub use p001_two_sum::*;
---
+++ rust/src/solutions/p011_container_water.rs
@@ -0,0 +1,42 @@
+// Container with Most Wwater
+
+pub fn max_area(height:Vec<i32>) -> i32 {
+    let mut res: i32 = 0;
+    
+    let mut l = 0;
+    let mut r = height.len() - 1;
+
+    while l < r {
+        let area = (r - l) as i32 * std::cmp::min(height[l], height[r]);
+        res = res.max(area);
+
+        if height[l] < height[r]{
+            l += 1;
+        }
+        else {
+            r -= 1;
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_container_water_1() {
+        let height = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
+        let result = max_area(height);
+        let expected = 49;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_container_water_2() {
+        let height = vec![1, 1];
+        let result = max_area(height);
+        let expected = 1;
+        assert_eq!(result, expected);
+    }
+}
