d547da68fafc1aee8931501d545c726762c81238
Nick <sop.theteachr@gmail.com>
2023-08-19 08:08:11 +0000

    :recycle: Refactor
    - Write a functional style solution
    - Remove duplication in test code

--- rust/src/solutions/p049_group_anagrams.rs
+++ rust/src/solutions/p049_group_anagrams.rs
@@ -1,50 +1,66 @@
 use std::collections::HashMap;
 
+const LETTER_COUNT: usize = 26;
+
+fn build_char_frequency(word: &String) -> [u8; LETTER_COUNT] {
+    word.bytes()
+        .map(|b| (b - b'a') as usize)
+        .fold([0; LETTER_COUNT], |mut freq, b| {
+            freq[b] += 1;
+            freq
+        })
+}
+
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    let mut res: HashMap<Vec<i32>, Vec<String>> = HashMap::new();
-
-    for s in strs.iter() {
-        let mut count = vec![0; 26];
-
-        for c in s.chars() {
-            count[(c as u8 - b'a') as usize] += 1
-        }
-        res.entry(count).or_insert(Vec::new()).push(s.clone());
-    }
-    res.values().cloned().collect()
+    strs.into_iter()
+        .map(|word| (build_char_frequency(&word), word))
+        .fold(
+            HashMap::<[u8; LETTER_COUNT], Vec<String>>::new(),
+            |mut anagrams, (char_freq, word)| {
+                anagrams.entry(char_freq).or_default().push(word);
+                anagrams
+            },
+        )
+        .into_values()
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    fn into_owned_strings(slices: Vec<&str>) -> Vec<String> {
+        slices.into_iter().map(String::from).collect()
+    }
+
     // To test correctly we need to sort
-    fn sort_string_vecs(mut vecs: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    fn sort_string_vecs(vecs: &mut Vec<Vec<String>>) {
         for vec in vecs.iter_mut() {
             vec.sort(); // Sort the strings within each group
         }
-        vecs.sort_by_key(|a| a.join("")); // Sort the groups
-        vecs
+        vecs.sort_by(|a, b| a.join("").cmp(&b.join(""))); // Sort the groups
     }
 
     #[test]
-    fn test_anagram_1() {
-        let strs = vec![
-            "eat".to_string(),
-            "tea".to_string(),
-            "tan".to_string(),
-            "ate".to_string(),
-            "nat".to_string(),
-            "bat".to_string(),
-        ];
-        let mut expected_output = vec![
-            vec!["bat".to_string()],
-            vec!["nat".to_string(), "tan".to_string()],
-            vec!["ate".to_string(), "eat".to_string(), "tea".to_string()],
-        ];
+    fn test_example_1() {
+        let strs = into_owned_strings(vec![
+            "eat",
+            "tea",
+            "tan",
+            "ate",
+            "nat",
+            "bat",
+        ]);
 
+        let mut expected_output = vec![
+            vec!["bat"],
+            vec!["nat", "tan"],
+            vec!["ate", "eat", "tea"],
+        ].into_iter().map(into_owned_strings).collect();
+        
         let mut result = group_anagrams(strs);
-        result = sort_string_vecs(result);
-        expected_output = sort_string_vecs(expected_output);
+
+        sort_string_vecs(&mut result);
+        sort_string_vecs(&mut expected_output);
 
         assert_eq!(result, expected_output);
     }
