118783d2dae28db5f0ee8399933b3680b8c079b7
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-19 21:07:12 +0530

    Problem 739

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -14,6 +14,7 @@ pub mod p167_two_sum_ii;
 pub mod p217_contains_duplicates;
 pub mod p238_product_of_array;
 pub mod p242_valid_anagram;
+pub mod p739_daily_temp;
 
 // Re-export the modules for external use
 //pub use p001_two_sum::*;
