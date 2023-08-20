8f81ef8aff0c085ab028848b8669f9fd3d9e6b24
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-20 22:16:11 +0530

    Co-authored-by: Nick <sop.theteachr@gmail.com>

--- Python/Linked_List/list_node.py
+++ Python/Linked_List/list_node.py
@@ -1,12 +1,23 @@
+from typing import Optional, Self
 class ListNode:
     def __init__(self, val=0, next=None) -> None:
         self.val = val
         self.next = next
-    
+        
     def insert(self, val):
         new_node = ListNode(val)
         new_node.next = self.next
         self.next = new_node
+
+    @classmethod
+    def from_list(cls, list_) -> Optional[Self]:
+        if not list_:
+            return None
+
+        if len(list_) == 1:
+            return cls(list_[0], None)
+
+        return cls(list_[0], cls.from_list(list_[1:]))
     
     @staticmethod
     def list_to_linked_list(lst):
@@ -21,6 +32,16 @@ class ListNode:
             cur = cur.next
         
         return head
+
+    def __str__(self) -> str:
+        temp = self
+        vals = []
+
+        while temp:
+            vals.append(str(temp.val))
+            temp = temp.next
+
+        return ' -> '.join(vals)
     
     @staticmethod
     def linked_list_to_list(head):
