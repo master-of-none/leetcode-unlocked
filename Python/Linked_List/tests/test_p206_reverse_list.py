a2a420a384e7711a49c60dfe78688b2afe37a6f4
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-20 11:35:59 +0530

    Changed Some things

--- Python/Linked_List/tests/test_p206_reverse_list.py
+++ Python/Linked_List/tests/test_p206_reverse_list.py
@@ -7,20 +7,40 @@ from list_node import ListNode
 
 class TestSolution:
     
-    @pytest.fixture
-    def solution(self):
-        return Solution
-    
-    def test_reverse_list(self, solution):
+    def test_reverse_list_1(self):
+        solution = Solution()
         input_list = [1, 2, 3, 4, 5]
         expected_list = [5, 4, 3, 2, 1]
         
         input_head = ListNode.list_linked_list(input_list)
         
-        actual_head = solution.reverse_list(self, input_head)
+        actual_head = solution.reverse_list(input_head)
         actual_list = ListNode.linked_list_to_list(actual_head)
           
         assert actual_list == expected_list
 
+    def test_reverse_list_2(self):
+        solution = Solution()
+        input_list = [1, 2]
+        expected_list = [2, 1]
+        
+        input_head = ListNode.list_linked_list(input_list)
+        
+        actual_head = solution.reverse_list(input_head)
+        actual_list = ListNode.linked_list_to_list(actual_head)
+        
+        assert actual_list == expected_list
+    
+    def test_reverse_list_3(self):
+        solution = Solution()
+        input_list = []
+        expected_list = []
+        
+        input_head = ListNode.list_linked_list(input_list)
+        
+        actual_head = solution.reverse_list(input_head)
+        actual_list = ListNode.linked_list_to_list(actual_head)
+        
+        assert actual_list == expected_list
 if __name__ == "__main__":
     pytest.main()
