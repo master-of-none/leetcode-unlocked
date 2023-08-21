a373c439d8dbcc642ab791bea480323b7f96898c
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-21 21:15:19 +0530

    Tried golang implementation for fun 🤪

---
+++ golang/Arrays/contains_duplicate/contains_dup.go
@@ -0,0 +1,13 @@
+package containsduplicate
+
+func containsDuplicate(nums []int) bool {
+	hashset := make(map[int]bool)
+
+	for _, n := range nums {
+		if hashset[n] {
+			return true
+		}
+		hashset[n] = true
+	}
+	return false
+}
