095290999de2b1672cb5fe104bb20c5d55b8433c
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-22 10:51:29 +0530

    edited

--- golang/Arrays/contains_duplicate/contains_dup.go
+++ golang/Arrays/contains_duplicate/contains_dup.go
@@ -11,3 +11,4 @@ func containsDuplicate(nums []int) bool {
 	}
 	return false
 }
+
--- golang/Arrays/valid_anagram/valid_anagram_test.go
+++ golang/Arrays/valid_anagram/valid_anagram_test.go
@@ -3,9 +3,9 @@ package validanagram
 import "testing"
 
 func TestIsAnagram(t *testing.T) {
-	tests := [] struct {
-		s string
-		t string
+	tests := []struct {
+		s        string
+		t        string
 		expected bool
 	}{
 		{"anagram", "nagaram", true},
@@ -13,11 +13,11 @@ func TestIsAnagram(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		t.Run(test.s + "_" +test.t, func(t *testing.T) {
+		t.Run(test.s+"_"+test.t, func(t *testing.T) {
 			result := isAnagram(test.s, test.t)
 
 			if result != test.expected {
-				t.Errorf("For %s and %s, expected %v, but %v is present", test.s, test.t,test.expected, result)
+				t.Errorf("For %s and %s, expected %v, but %v is present", test.s, test.t, test.expected, result)
 			}
 		})
 	}
