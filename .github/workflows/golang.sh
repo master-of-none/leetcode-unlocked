0fbbad24b4b75ee7d42a21bb8bec103e74cf5be1
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-22 09:45:03 +0530

    Update golang.sh

--- .github/workflows/golang.sh
+++ .github/workflows/golang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for dir in $(find . -type d -name '*'); do
-    if [[-f "$dir/go.mod"]]; then
+    if [[ -f "$dir/go.mod" ]]; then
         echo "Running Tests in Directory: $dir"
         cd "$dir"
         go test -v ./...
